f26cd12616efec91a4d14f5aae1f94025a3583a6
musicjerm <musicjerm@gmail.com>
2018-04-02 17:25:16 -0600

    initial commit - test controller, route and view

---
+++ Controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Musicjerm\Bundle\JermBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+class BaseController extends Controller
+{
+    /**
+     * @param UserInterface $user
+     * @param $entity
+     * @return Response
+     */
+    public function indexAction(UserInterface $user, $entity)
+    {
+
+        return $this->render('JermBundle:Base:index.html.twig', array(
+            'test_entity' => $entity
+        ));
+    }
+}
