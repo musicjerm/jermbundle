4f75b28b3ab9e9db46ca427bc6e8ca74b6248da6
musicjerm <musicjerm@gmail.com>
2018-04-02 17:56:53 -0600

    add bundle file

---
+++ JermBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Musicjerm\Bundle\JermBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony Bundle
+ *
+ * @author Jeremy Antunez https://github.com/musicjerm/jermbundle
+ */
+class JermBundle extends Bundle
+{
+
+}
