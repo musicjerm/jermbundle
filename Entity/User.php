ee5125c4ebca01b5d4f6fcef15dd5e983ba6218a
musicjerm <musicjerm@gmail.com>
2018-05-16 13:58:22 -0600

    added base user entity that implements UserInterface

---
+++ Entity/User.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Musicjerm\Bundle\JermBundle\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class User implements UserInterface, \Serializable
+{
+    /** @var int */
+    protected $id;
+
+    /** @var string */
+    protected $username;
+
+    /** @var string */
+    protected $password;
+
+    /** @var string */
+    protected $email;
+
+    /** @var array */
+    protected $roles;
+
+    public function __construct()
+    {
+        $this->roles = array();
+    }
+
+    public function __toString(): string
+    {
+        return $this->getUsername();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = password_hash($password, PASSWORD_BCRYPT);
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+
+    public function eraseCredentials(): void{}
+
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password
+        ));
+    }
+
+    public function unserialize($serialized): void
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password
+            ) = unserialize($serialized);
+    }
+
+    public function getSalt()
+    {
+        //handled by BCRYPT
+        return null;
+    }
+}
