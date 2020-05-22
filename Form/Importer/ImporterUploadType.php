f54af6e9e25a14b1760299e4c0ddfdec345f81d9
Jeremy Antunez <musicjerm@gmail.com>
2020-05-22 11:24:50 -0600

    Use FileType class instead of letting Symfony guess the form field type.

--- Form/Importer/ImporterUploadType.php
+++ Form/Importer/ImporterUploadType.php
@@ -3,6 +3,7 @@
 namespace Musicjerm\Bundle\JermBundle\Form\Importer;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -10,7 +11,7 @@ class ImporterUploadType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('file', null, ['label' => 'Select CSV file']);
+        $builder->add('file', FileType::class, ['label' => 'Select CSV file']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
