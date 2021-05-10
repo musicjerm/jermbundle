94c9aeaa3ee471d01a62e83512ccab95e836019d
Jeremy Antunez <musicjerm@gmail.com>
2021-05-10 17:25:35 -0600

    add unique dto validator for use with edit / update forms

---
+++ Validator/Constraints/UniqueDTOedit.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Musicjerm\Bundle\JermBundle\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class UniqueDTOedit extends Constraint
+{
+    public $message = 'This value is already used.';
+    public $errorPath;
+    public $entityClass;
+    public $ignoreNull = true;
+    public $fields = array();
+
+    public function getRequiredOptions(): array
+    {
+        return ['entityClass', 'fields'];
+    }
+
+    public function validatedBy(): string
+    {
+        return \get_class($this) . 'Validator';
+    }
+
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ Validator/Constraints/UniqueDTOeditValidator.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Musicjerm\Bundle\JermBundle\Validator\Constraints;
+
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class UniqueDTOeditValidator extends ConstraintValidator
+{
+    private $registry;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->registry = $registry;
+    }
+
+    /**
+     * @param object $value
+     * @param Constraint|UniqueDTO $constraint
+     */
+    public function validate($value, Constraint $constraint): void
+    {
+        $fields = (array) $constraint->fields;
+        $repo = $this->registry->getRepository($constraint->entityClass);
+        $criteria = array();
+
+        foreach ($fields as $fieldName){
+            if ($constraint->ignoreNull && $value->$fieldName === null){
+                continue;
+            }
+
+            $criteria[$fieldName] = $value->$fieldName;
+        }
+
+        if (empty($criteria)){
+            return;
+        }
+
+        $result = $repo->{'findBy'}($criteria);
+
+        $em = $this->registry->getManager();
+        $identifier = $em->getClassMetadata($constraint->entityClass)->getIdentifier()[0];
+        $idGetter = 'get' . ucfirst($identifier);
+
+        if (\count($result) === 0){
+            return;
+        }
+
+        if (\count($result) === 1 && $result[0]->$idGetter() === $value->$identifier){
+            return;
+        }
+
+        $this->context->buildViolation($constraint->message)
+            ->atPath($constraint->errorPath)
+            ->setParameter('{{ string }}', $value)
+            ->addViolation();
+    }
+}
